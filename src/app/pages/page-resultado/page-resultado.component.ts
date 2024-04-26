ad9b236cdf0cc027b32c137661921bb104e2078b
Noemi Rosario Ancari Villcarani <nancari@ine.gov.bo>
2024-04-26 18:43:40 -0400

    adicion de grafico

--- src/app/pages/page-resultado/page-resultado.component.ts
+++ src/app/pages/page-resultado/page-resultado.component.ts
@@ -19,28 +19,28 @@ import { ResultadoService } from './resultado.service';
 @Component({
   selector: 'app-page-resultado',
   standalone: true,
-  imports: [CommonModule,ButtonModule,TabMenuModule,AgGridAngular],
+  imports: [CommonModule, ButtonModule, TabMenuModule, AgGridAngular],
   templateUrl: './page-resultado.component.html',
   styleUrl: './page-resultado.component.scss'
 })
 export class PageResultadoComponent {
   private chart: am4charts.XYChart;
   informacion: any;
-  data:any;
+  data: any;
 
   items: MenuItem[] | undefined;
 
   rowData = [];
   rowDataTotal = [];
- 
+
   // Column Definitions: Defines the columns to be displayed.
   colDefs: ColDef[] = [];
-  selectedTab: string = 'Gráfico'; // Inicialmente mostrando el gráfico
+  selectedTab: string = 'Tabla'; // Inicialmente mostrando el gráfico
   activeItem: MenuItem | undefined;
 
- 
 
-  constructor(@Inject(PLATFORM_ID) private platformId, private zone: NgZone,private router: Router, private informacionService:InformacionService, private resultadoService :ResultadoService) {}
+
+  constructor(@Inject(PLATFORM_ID) private platformId, private zone: NgZone, private router: Router, private informacionService: InformacionService, private resultadoService: ResultadoService) { }
 
   // Run the function only in the browser
   browserOnly(f: () => void) {
@@ -52,66 +52,29 @@ export class PageResultadoComponent {
   }
 
   ngOnInit() {
-   
     this.informacion = this.informacionService.informacion;
     console.log(' this.informacion', this.informacion)
-    if(this.informacion.paso1Informacion.selectedFlujo && this.informacion.paso2Informacion ){
+    this.grafico(this.informacion);
+    if (this.informacion.paso1Informacion.selectedFlujo && this.informacion.paso2Informacion) {
       this.getResultado(this.informacion);
+      this.grafico(this.informacion);
 
-    }else{
+    } else {
       this.prevPage();
     }
 
     this.items = [
-      { label: 'Gráfico', icon: 'pi pi-fw pi-chart-bar' },
       { label: 'Tabla', icon: 'pi pi-fw pi-table' },
-  ];
+      { label: 'Gráfico', icon: 'pi pi-fw pi-chart-bar' },
+    ];
   }
 
-  ngAfterViewInit() {
-    // Chart code goes in here
-    this.browserOnly(() => {
-      am4core.useTheme(am4themes_animated);
 
-      let chart = am4core.create("chartdiv", am4charts.XYChart);
-
-      chart.paddingRight = 20;
-
-      let data = [];
-      let visits = 10;
-      for (let i = 1; i < 366; i++) {
-        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-        data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
-      }
-
-      chart.data = data;
-
-      let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-      dateAxis.renderer.grid.template.location = 0;
-
-      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-      valueAxis.tooltip.disabled = true;
-      valueAxis.renderer.minWidth = 35;
-
-      let series = chart.series.push(new am4charts.LineSeries());
-      series.dataFields.dateX = "date";
-      series.dataFields.valueY = "value";
-      series.tooltipText = "{valueY.value}";
-
-      chart.cursor = new am4charts.XYCursor();
-
-      let scrollbarX = new am4charts.XYChartScrollbar();
-      scrollbarX.series.push(series);
-      chart.scrollbarX = scrollbarX;
-
-      this.chart = chart;
-    });
-  }
 
   prevPage(): void {
     console.log('Todos los campos están llenos. Pasando a la siguiente página...');
     this.router.navigate(['/consult/data/paso3']);
-}
+  }
 
   ngOnDestroy() {
     // Clean up chart when the component is removed
@@ -124,24 +87,89 @@ export class PageResultadoComponent {
 
   onActiveItemChange(event: MenuItem) {
     this.activeItem = event;
-    if(this.activeItem.label === 'Gráfico')
-    this.selectedTab = 'Gráfico';
-    if(this.activeItem.label === 'Tabla')
+    if (this.activeItem.label === 'Gráfico'){
+      this.selectedTab = 'Gráfico';
+      this.grafico( this.rowData);
+    }
+     
+    if (this.activeItem.label === 'Tabla')
       this.selectedTab = 'Tabla';
   }
   /***metodo para enviar datos***/
 
-  getResultado(resultado:any): void {
+  getResultado(resultado: any): void {
     this.resultadoService.getResultado(resultado).subscribe(
       (resultado) => {
         this.data = resultado;
         this.colDefs = this.data.colDefs;
         this.rowData = this.data.result;
         this.rowDataTotal = this.data.resultTotales;
+        this.grafico( this.rowData);
+
       },
       (error) => {
         console.error('Error al obtener los cualitativas:', error);
       }
     );
   }
+
+  grafico(data:any):void {
+
+    console.log('llamando a la fncion grafico-------------')
+
+      // Chart code goes in here
+      this.browserOnly(() => {
+        am4core.useTheme(am4themes_animated);
+        //am4core.useTheme(am4themes_kelly);
+  
+        // Create chart instance
+        var chart = am4core.create("chartdiv", am4charts.XYChart);
+  
+        chart.marginRight = 400;
+  
+        // Add data
+        chart.data = data;
+        // Create axes
+        var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "gestion";
+        categoryAxis.title.text = "Local gestion offices";
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.minGridDistance = 20;
+  
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.title.text = "Expenditure (M)";
+  
+        // Create series
+        var series = chart.series.push(new am4charts.ColumnSeries());
+        series.dataFields.valueY = "sum_e_kilbru";
+        series.dataFields.categoryX = "gestion";
+        series.name = "sum_e_kilbru";
+        series.tooltipText = "{name}: [bold]{valueY}[/]";
+        // This has no effect
+        // series.stacked = true;
+  
+        var series2 = chart.series.push(new am4charts.ColumnSeries());
+        series2.dataFields.valueY = "sum_e_kilnet";
+        series2.dataFields.categoryX = "gestion";
+        series2.name = "sum_e_kilnet";
+        series2.tooltipText = "{name}: [bold]{valueY}[/]";
+        // Do not try to stack on top of previous series
+        // series2.stacked = true;
+  
+        var series3 = chart.series.push(new am4charts.ColumnSeries());
+        series3.dataFields.valueY = "sum_e_valor";
+        series3.dataFields.categoryX = "gestion";
+        series3.name = "sum_e_valor";
+        series3.tooltipText = "{name}: [bold]{valueY}[/]";
+        series3.stacked = true;
+  
+        // Add cursor
+        chart.cursor = new am4charts.XYCursor();
+  
+        // Add legend
+        chart.legend = new am4charts.Legend();
+      });
+
+  }
+   
 }
